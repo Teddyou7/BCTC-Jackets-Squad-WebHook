8c8967d2a23a9f4b5bb7f004061c3a7b3b2d626d
Teddyou <68055574+Teddyou7@users.noreply.github.com>
2023-11-17 23:54:34 +0800

    Update config.sh

--- cfg/config.sh
+++ cfg/config.sh
@@ -1,5 +1,5 @@
 #注意，此项目中附带了一个已经编译好的WebHook程序，如果您对此程序不放心，可自行编译替换，项目地址：https://github.com/adnanh/webhook
-#其使用的RCON程序也是编译好的，如果不放心可审核源码后自行编译，源码为：https://github.com/Teddyou7/Squad-HelicopterTraining/blob/main/rcon.c
+#其使用的RCON程序也是编译好的，如果不放心可审核源码后自行编译，源码为：https://github.com/Teddyou7/BCTC-Jackets-Squad-WebHook/blob/main/bin/rcon/rcon.c
 #其使用的MCRCON源码为：https://github.com/Tiiffi/mcrcon
 #
 #正确使用此包需要配置BattleMetrics的触发器！由触发器决定脚本调用，功能脚本注释中将会提示传参格式，或使用后续建设的文档
