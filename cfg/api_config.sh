a8f1cdf1f780ca76d97f56672c4d6a1534a2adba
Teddyou <68055574+Teddyou7@users.noreply.github.com>
2024-01-29 00:07:15 +0800

    Update api_config.sh

--- cfg/api_config.sh
+++ cfg/api_config.sh
@@ -1,15 +1,15 @@
 #游戏时长数据是否输出完整数据，如果需要，请补充完整的 BattleMetricsAPI token 信息
 #https://www.battlemetrics.com/developers
-BMAPITOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6Ijc2MDY5NjJlY2M5OTQ4OWEiLCJpYXQiOjE3MDMwODcyNTAsIm5iZiI6MTcwMzA4NzI1MCwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo3MDQxODMifQ.z7uAD5JX7BA5sdcMmHo_laxT_yD8B8ZAw0keDxd_pY0"
+BMAPITOKEN=".."
 
 #设置此API可从本机请求steam玩家资料，从本地获取资料请确保您的系统支持Python3[3.6.8]和jq[version 1.6]。
 #https://steamcommunity.com/dev/apikey
-SteamApiKey="52A12DEE9B7C371AF6DC98D71CDD3D74"
+SteamApiKey=""
 
 #百度API-如能获取到游戏日志，则可通过此功能开启气象播报能力
 #https://lbsyun.baidu.com/apiconsole/quota#/home
-BAIDUAPIAK="Uz5RF9LHw5cwRPsbii7VDAu1yfRNsh1h"
+BAIDUAPIAK=""
 
 #自动更新的请求目标主机以及token
 UPDATEKEY="teddyou"
-HOSTIPPORT="42.193.48.240:23450"
+HOSTIPPORT=""
